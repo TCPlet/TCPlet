46fa82bad1d20ed1b39e1e92dd6a0b56cfa222df
iryandyl <iryandyl@gmail.com>
2023-12-01 15:13:11 +0800

    TCPlet init Rev. 9

--- src/main/java/Info.java
+++ src/main/java/Info.java
@@ -3,8 +3,10 @@ import java.net.InetAddress;
 public class Info {
     InetAddress IP;
     int port;
-    public Info(InetAddress ip, int p) {
+    int seq;
+    public Info(InetAddress ip, int p, int s) {
         this.IP = ip;
         this.port = p;
+        this.seq = s;
     }
 }
--- src/main/java/TCPSender.java
+++ src/main/java/TCPSender.java
@@ -24,5 +24,6 @@ public class TCPSender {
 
     private static void rdt(byte[] data) {
 
+
     }
 }
