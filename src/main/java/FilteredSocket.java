8aa84a007e2be3366b58b9a7ee3190b747a5a630
3414114 <221250019@smali.nju.edu.cn>
2023-12-02 19:29:00 +0800

    simulation

--- src/main/java/FilteredSocket.java
+++ src/main/java/FilteredSocket.java
@@ -1,13 +1,27 @@
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.*;
 
 public class FilteredSocket {
 
     private final DatagramSocket socket;
 
+    //PriorityQueue Compare
+    Comparator<BufferNode> customComparator = new Comparator<BufferNode>() {
+        @Override
+        public int compare(BufferNode o1, BufferNode o2) {
+            // 根据元素的值进行比较，值越小优先级越高
+            //根据checksum比较
+            int num_o1=o1.data[18]+o1.data[19];
+            int num_o2=o2.data[18]+o2.data[19];
+            return Integer.compare(num_o1,num_o2);
+        }
+    };
+    private PriorityQueue<BufferNode> buffer=new PriorityQueue<>(customComparator);
     public FilteredSocket(int port) {
         try {
             socket = new DatagramSocket(port);
@@ -40,21 +54,80 @@ public class FilteredSocket {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        checkBuffer();
     }
-
+    public void checkBuffer(){
+        while (!buffer.isEmpty()){
+            BufferNode temp=buffer.poll();
+            rawChannelSend(temp.data,temp.dest,temp.port);
+        }
+    }
     // data loss
     public void lossyChannelSend(byte[] data, InetAddress dest, int destPort) {
         //TODO
+        if(smallChance())return;
+        rawChannelSend(data,dest,destPort);
     }
 
     // data corruption
     public void noisyChannelSend(byte[] data, InetAddress dest, int destPort) {
         //TODO
+        if(smallChance()){
+            if(halfChance()){
+                //corrupt content
+                changeContent(data);
+            }
+            if(halfChance()){
+                //change order
+                BufferNode Node=new BufferNode(data,dest,destPort);
+                buffer.add(Node);
+                return;
+            }
+
+        }
+        rawChannelSend(data,dest,destPort);
     }
 
     // data loss and corruption
     public void noisyAndLossyChannelSend(byte[] data, InetAddress dest, int destPort) {
         //TODO
+        if(halfChance()){
+            noisyChannelSend(data,dest,destPort);
+        } else{
+            lossyChannelSend(data,dest,destPort);
+        }
     }
 
+    private byte[]changeContent(byte[]data){
+        int len=data.length*0.01>1?(int)(data.length*0.01):1;
+        Queue<Integer>Numqueue=new LinkedList<>();
+        Random random=new Random();
+        for (int i=0;i<len;i++)Numqueue.add(random.nextInt(data.length));
+        while (!Numqueue.isEmpty()){
+            int temp=random.nextInt(8);
+            byte mask = (byte)(1 << temp);// eg:00001000
+            int index=Numqueue.poll();
+            data[index]=(byte) (data[index]^mask&0xff);
+        }
+        return data;
+    }
+    private boolean smallChance(){
+        Random random=new Random();
+        Double chance=random.nextDouble(100);
+        return (chance<0.01);
+    }
+    private boolean halfChance(){
+        Random random=new Random();
+        return random.nextBoolean();
+    }
+    class BufferNode{
+        byte[]data;
+        InetAddress dest;
+        int port;
+        public BufferNode(byte[]data,InetAddress dest,int port){
+            this.data=data;
+            this.dest=dest;
+            this.port=port;
+        }
+    }
 }
