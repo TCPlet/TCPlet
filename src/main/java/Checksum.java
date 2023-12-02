f2c7919d9284f0ba5ed6fe0bc38a90aed3adc49d
3414114 <221250019@smali.nju.edu.cn>
2023-12-02 16:21:07 +0800

    first commit

--- src/main/java/Checksum.java
+++ src/main/java/Checksum.java
@@ -1,11 +1,32 @@
+import java.util.Arrays;
 public class Checksum {
     public static byte[] genChecksum(byte[] input) {
+        int value=0;
+        int len=input.length;
+        boolean isEven=input.length%2==0;
+        if(!isEven)len--;
+        for (int i=0;i<len;i+=2){
+            value+=((input[i]&0xff)<<8)+(input[i+1]&0xff);
+        }
+        if(!isEven)value+=(input[len]&0xff)<<8;
+        //the way don't use the first 16
+        byte[]result=new byte[2];
+        result[0]=(byte)(~(value>>8&0xff));
+        result[1]=(byte) (~(value&0xff));
         //TODO
-        return null;
+        return result;
     }
 
     public static boolean verifyChecksum(byte[] input) {
+        int value=0;
+        int len=input.length;
+        boolean isEven=input.length%2==0;
+        if(!isEven)len--;
+        for (int i=0;i<len;i+=2){
+            value+=((input[i]&0xff)<<8)+(input[i+1]&0xff);
+        }
+        if(!isEven)value+=(input[len]&0xff)<<8;
+        return ((value&0xffff)==0xffff);
         //TODO
-        return true;
     }
 }
